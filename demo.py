d002347a3f6b846b6c0e34d6b20a99b291e203d0
ajay <56541738+Ajaykolluri@users.noreply.github.com>
2019-11-11 10:38:26 +0530

    Delete demo.py

--- demo.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-connection = pymongo.MongoClient('localhost',27017)
-database = connection["chat"]
-collection = database["stu"]
-print("database connected")
-data = {'Name':"",'age':'','Email':"",'height':'','weight':'','Gender':""}
-collection.insert_one(data)
-connection.close()
