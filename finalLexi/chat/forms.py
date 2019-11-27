5edfa8e10342db5eb2df44f8533f53da36bb3616
GMadhusudanarao <56541780+GMadhusudanarao@users.noreply.github.com>
2019-11-27 13:02:41 +0530

    Delete forms.py

--- finalLexi/chat/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from chat.models import UserDetails
-
-class UserModelForm(ModelForm):
-    class Meta:
-        model = UserDetails
-        fields = ['name','age','gender','height','weight','smoke','drink','diabetes','highbp']
-
-       
