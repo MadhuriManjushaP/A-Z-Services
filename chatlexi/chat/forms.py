f107c849950db1ffd926ab79285ca4da9cab3bdc
GMadhusudanarao <56541780+GMadhusudanarao@users.noreply.github.com>
2019-12-18 11:12:47 +0530

    Update forms.py

--- chatlexi/chat/forms.py
+++ chatlexi/chat/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from chat.models import UserDetails
-
+#create form class and fields here for user registration
 class UserModelForm(ModelForm):
     class Meta:
         model = UserDetails
