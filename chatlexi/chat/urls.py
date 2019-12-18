c5c2c59731dc0723aadf759a09e72b129f7c6f8c
GMadhusudanarao <56541780+GMadhusudanarao@users.noreply.github.com>
2019-12-18 11:07:11 +0530

    Update urls.py

--- chatlexi/chat/urls.py
+++ chatlexi/chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.contrib import admin
-
+#import views here
 from . import views as chat_views
-
+#create path urls here
 urlpatterns = [
  path('', chat_views.userDetails),
  path('temp/', chat_views.userDetails),
